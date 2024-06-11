e4d09b1a329ba3464ad93084f5d128f92221b46b
Daniel Tashjian <dtashjian@inmarket.com>
2024-06-11 10:55:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+To witness bug (ctrl-c will fail to exit the program):
+
+```shell
+$ poetry update
+$ poetry run python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from causalimpact import CausalImpact
+
+data = pd.read_csv('https://raw.githubusercontent.com/WillianFuks/tfcausalimpact/master/tests/fixtures/arma_data.csv')[['y', 'X']]
+data.iloc[70:, 0] += 5
+
+pre_period = [0, 69]
+post_period = [70, 99]
+
+ci = CausalImpact(data, pre_period, post_period)
+print(ci.summary())
+print(ci.summary(output='report'))
+ci.plot()
